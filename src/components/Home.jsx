305e2bae86e443fc3bd8c05b28b9bee33f582178
crewszk <crewszk@outlook.com>
2020-08-18 19:16:08 -0400

    initial commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export const Home = () => (
     <div>
-        <h1>Hello World, this is the home page</h1>
+        <h1>Hello World, this is the home page.</h1>
+        <h2>THIS WEBSITE IS CURRENTLY UNDER CONSTRUCTION</h2>
     </div>
 )
