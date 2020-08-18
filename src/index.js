4c9d2365769879bf1f6ca918e877c2cc768dc9f5
crewszk <crewszk@outlook.com>
2020-08-18 19:08:01 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { App } from './components/App';
+import { App } from './App';
 
 ReactDOM.render(App, document.getElementById('root'));
