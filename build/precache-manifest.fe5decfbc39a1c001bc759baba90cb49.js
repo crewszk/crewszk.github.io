f6750dff7056e6cc879ed5d64d77460184e834f8
crewszk <crewszk@outlook.com>
2020-08-20 22:04:22 -0400

    comitting gh-pages build

---
+++ build/precache-manifest.fe5decfbc39a1c001bc759baba90cb49.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "692dfcd4c028fa9a425c1957019aebf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f60c5974ba318e1cd47",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "5f60c5974ba318e1cd47",
+    "url": "/static/js/2.7c0c273f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.7c0c273f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6fd9ef4e8e60d6ab820",
+    "url": "/static/js/main.056dcba0.chunk.js"
+  },
+  {
+    "revision": "4d030bf2aa0c41b6f6cf",
+    "url": "/static/js/runtime-main.ccef853a.js"
+  },
+  {
+    "revision": "6ff2794e89ff4260f4f12318395cc7ad",
+    "url": "/static/media/construction.6ff2794e.gif"
+  },
+  {
+    "revision": "ea216ffd5164d8cd993a35542c63bfb2",
+    "url": "/static/media/myAvatar.ea216ffd.jpg"
+  },
+  {
+    "revision": "5d70d38dd0e766ebd60b8d747ea3cb78",
+    "url": "/static/media/sclassSmallGif.5d70d38d.gif"
+  }
+]);
