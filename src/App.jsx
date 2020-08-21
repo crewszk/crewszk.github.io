2be7f3b721f8b39cd455efd21ee564cebb1a0485
crewszk <crewszk@outlook.com>
2020-08-20 22:07:09 -0400

    Added navigation component and material-ui support

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Home } from './components/Home';
-import { Projects } from './components/Projects';
-import { Contact } from './components/Contact';
-import { NoMatch } from './components/NoMatch';
-
+import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Projects } from "./pages/Projects";
+import { About } from "./pages/About";
+import { NoMatch } from "./pages/NoMatch";
+import Navigation from "./components/Navigation";
 
 class App extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Router>
-                    <Switch>
-                        <Route exact path="/" component ={Home} />
-                        <Route path="/projects" component ={Projects} />
-                        <Route path="/contact" component ={Contact} />
-                        <Route component ={NoMatch} />
-                    </Switch>
-                </Router>
-            </React.Fragment>
-        )
-    }
+  state = {
+    pages: ["/projects", "/school", "/about"],
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Navigation />
+        <Grid>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/projects" component={Projects} />
+              <Route path="/about" component={About} />
+              <Route component={NoMatch} />
+            </Switch>
+          </Router>
+        </Grid>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
