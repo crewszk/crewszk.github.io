7f9e4b3d64b25a19cd57618deb13cafc1bd604e8
crewszk <crewszk@outlook.com>
2020-08-23 18:12:49 -0400

    deployed build to ghpages

---
+++ build/precache-manifest.58597a11ba8b7bfd48289888cadf3eca.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0bf74e765b62ebb8dc490f3d27b09c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "521af3bc1a86c2ac21cc",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "521af3bc1a86c2ac21cc",
+    "url": "/static/js/2.1a128bee.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.1a128bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3630f20d7d82e9891f82",
+    "url": "/static/js/main.a82b360e.chunk.js"
+  },
+  {
+    "revision": "4d030bf2aa0c41b6f6cf",
+    "url": "/static/js/runtime-main.ccef853a.js"
+  },
+  {
+    "revision": "6ff2794e89ff4260f4f12318395cc7ad",
+    "url": "/static/media/construction.6ff2794e.gif"
+  },
+  {
+    "revision": "ea216ffd5164d8cd993a35542c63bfb2",
+    "url": "/static/media/myAvatar.ea216ffd.jpg"
+  }
+]);
